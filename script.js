12542f86885024d56cbb0fc9a7a8b6de47e18125
danielz0102 <danyzglez@hotmail.com>
2024-10-06 00:47:26 -0600

    Add initial functions

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let firstNumber = null, secondNumber = null, operator = null
+})
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function operate(firstNumber, secondNumber, operator) {
+    const functions = {
+        '+': add,
+        '-': subtract,
+        'x': multiply,
+        '÷': divide,
+    }
+
+    const selectedFunc = functions[operator]
+    selectedFunc(firstNumber, secondNumber)
+}
