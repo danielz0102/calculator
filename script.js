8db5a630ae457243b7211c23c19a5836c351f7e6
danielz0102 <danyzglez@hotmail.com>
2024-10-06 22:43:16 -0600

    Update display when user clicks a button

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let firstNumber = null, secondNumber = null, operator = null
+    let firstNumber = null, secondNumber = null, operator = null, display = null
+    const displayBtns = document.querySelectorAll('.btn-show')
 
+    displayBtns.forEach(btn => {
+        btn.addEventListener('click', e => {
+            display = updateDisplay(e)
+        })
+    })
 })
 
 function add(a, b) {
@@ -29,4 +35,10 @@ function operate(firstNumber, secondNumber, operator) {
 
     const selectedFunc = functions[operator]
     selectedFunc(firstNumber, secondNumber)
+}
+
+function updateDisplay(event) {
+    const display = document.querySelector('.display')
+    display.textContent += event.target.textContent
+    return display.textContent
 }
